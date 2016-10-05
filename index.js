db471bea09c6a2c8eb2644c4d78a1ef4c4633f26
Anthony Andriolas <adonie@gmail.com>
2016-10-05 16:47:37 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function whisper(string){
 function logShout(string){
   console.log();
 }
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
